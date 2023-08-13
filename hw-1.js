705620bf802ad5c835ade24cf4fd165dc02058ec
Anna <anko183@yandex.ru>
2023-08-14 01:43:57 +0300

    2 hw

---
+++ hw-1.js
@@ -0,0 +1,37 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let iPhone = 2007;
+alert(iPhone);
+
+let js = `Брендан Айк`;
+alert(js);
+
+let number1 = 10;
+let number2 = 2;
+
+alert(number1+number2);
+alert(number1-number2);
+alert(number1*number2);
+alert(number1/number2);
+
+let result = alert(2 ** 5);
+
+a = 9;
+let b = 2;
+alert(a % b);
+
+let num = 1;
+num+=5;
+num-=3;
+num*=7;
+num/=3;
+num+=1;
+num-=1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
