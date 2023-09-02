5d40eba833b9c262ddf77b5fb7eae9ca2060b66d
Anna <anko183@yandex.ru>
2023-09-02 13:52:20 +0300

    changed

--- hw-3.js
+++ hw-3.js
@@ -32,42 +32,26 @@ alert(a + b);
 
 let monthNumber = prompt('введите месяц');
 switch (monthNumber) {
+    case '12':
     case '1':
-            alert('Зима')
-        break;
     case '2':
             alert('Зима')
             break;    
-    case '3':
-            alert('Весна')
-            break;    
-    case '4':
-            alert('Весна')
-            break;    
+    case '3':   
+    case '4':  
     case '5':
             alert('Весна')
             break;   
     case '6':
-            alert('Лето')
-                break;  
-    case '7':
-            alert('Лето')
-            break;    
+    case '7':  
     case '8':
             alert('Лето')
             break;    
-    case '9':
-            alert('Осень')
-            break;    
-    case '10':
-            alert('Осень')
-            break;    
+    case '9':  
+    case '10':    
     case '11':
             alert('Осень')
-            break;    
-    case '12':
-            alert('Зима')
-            break;    
+            break;       
     default:
         alert('чиво')
         break;
