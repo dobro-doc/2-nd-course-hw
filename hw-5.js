b85d1e91522c7f18d3590661ba05c33ab25e2e64
Anna <anko183@yandex.ru>
2023-09-26 00:56:16 +0300

    changed

--- hw-5.js
+++ hw-5.js
@@ -66,3 +66,17 @@ function numSquare() {
     getPerimeter: getCirclePerimeter,
   };
 
+  function calcSeason() {
+    let month = Number(prompt("Какой сейчас месяц?"));
+    if(month = 12 || month = 1 || month = 2) {
+      return 'Зима';
+    } else if (month = 3 || month = 4 || month = 5) {
+      return 'Весна';
+    } else if (month = 6 || month = 7 || month = 8) {
+      return 'Лето';
+    } else if (month = 9 || month = 10 || month = 11) {
+      return 'Осень';
+    } else() {
+      return 'Значение введено некорректно';
+    }
+  }
