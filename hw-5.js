447389917b9402c0f1c62087dccdf04e56789979
Anna <anko183@yandex.ru>
2023-09-26 00:57:57 +0300

    changed

--- hw-5.js
+++ hw-5.js
@@ -70,11 +70,11 @@ function numSquare() {
     let month = Number(prompt("Какой сейчас месяц?"));
     if(month = 12 || month = 1 || month = 2) {
       return 'Зима';
-    } else if (month = 3 || month = 4 || month = 5) {
+    } else if(month = 3 || month = 4 || month = 5) {
       return 'Весна';
-    } else if (month = 6 || month = 7 || month = 8) {
+    } else if(month = 6 || month = 7 || month = 8) {
       return 'Лето';
-    } else if (month = 9 || month = 10 || month = 11) {
+    } else if(month = 9 || month = 10 || month = 11) {
       return 'Осень';
     } else() {
       return 'Значение введено некорректно';
